a37fa1f5fc0ed61f28dfc1bc206f6ed1cb1ed1bc
Alex Casalboni <alexsaxmib@gmail.com>
2016-05-24 12:33:38 +0200

    Added new "continent" function (example)

---
+++ restApi/continent/handler.py
@@ -0,0 +1,19 @@
+"""
+ Serverless Module: Lambda Handler
+ - Your lambda functions should be a thin wrapper around your own separate
+   modules, to keep your code testable, reusable and AWS independent
+"""
+
+from __future__ import print_function
+
+import json
+import logging
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../"))
+import lib
+
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+def handler(event, context):
+    return lib.continent_by_country_name(event)
